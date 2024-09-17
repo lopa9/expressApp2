707490c07810ab686732cf35ac5fb3832beb8e42
lopa9 <lopamudra.p188@gmail.com>
2024-09-17 14:16:44 +0000

    added getBookById, Update and Delete

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -5,5 +5,9 @@ const bookController = require('../controllers/bookController');
 
 router.post('/books',bookController.createBook);
 router.get('/books', bookController.getAllBooks);
+router.get('/books/:id', bookController.getBookById);
+router.put('/books/:id', bookController.updateBook);
+router.delete('/books/:id', bookController.deleteBook);
+
 
 module.exports = router;
